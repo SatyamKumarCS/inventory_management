37dfaae27c196267648d6660d83ca33d8ddb8468
SatyamKumarCS <satyamkumarch15@gmail.com>
2025-06-13 03:57:17 +0530

    Adding Buttons

--- client/src/pages/ItemMaster/page.jsx
+++ client/src/pages/ItemMaster/page.jsx
@@ -1,22 +1,25 @@
-
 import Sidebar from "../../components/sidebar/page";
 import Header from "../../components/header/page";
 import './style.css'; 
 
 const ItemMaster = () => {
-    // const [showAddCategory, setShowAddCategory] = useState(false);
-    // const [showAddSubCategory, setShowAddSubCategory] = useState(false);
-    // const [showItemForm, setShowItemForm] = useState(false);
   return (
     <div className="ItemMaster-container">
       <Sidebar />
       <div className="ItemMaster-main">
         <Header />
-      <div className="ItemMaster-content">
-        <h1>Item Master</h1>
+        <div className="ItemMaster-content">
+          <div className="ItemMaster-header-row">
+            <h1>Item Master</h1>
+            <div className="ItemMaster-buttons">
+              <button className="action-btn">Add Category</button>
+              <button className="action-btn">Add Sub Category</button>
+              <button className="action-btn">Add Item</button>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
